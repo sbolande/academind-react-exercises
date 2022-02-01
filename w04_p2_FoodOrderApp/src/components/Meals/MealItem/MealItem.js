69609e24f6569cff09baa4435caa30b0d4554624
Seth Bolander <bol15015@byui.edu>
2022-01-31 19:22:31 -0700

    updated use of context consumer into MealItemForm

--- w04_p2_FoodOrderApp/src/components/Meals/MealItem/MealItem.js
+++ w04_p2_FoodOrderApp/src/components/Meals/MealItem/MealItem.js
@@ -1,7 +1,21 @@
+import { useContext } from 'react';
+
+import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
+import CartContext from '../../../store/cart-context';
 
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
   const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = amount => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price
+    });
+  };
 
   return (
     <li className={classes.meal}>
@@ -10,7 +24,9 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
+      </div>
     </li>
   );
 };
