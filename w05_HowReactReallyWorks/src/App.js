0f92f84a59ecdb239df2eaa691fb71a156106529
Seth Bolander <bol15015@byui.edu>
2022-02-05 16:55:59 -0700

    Refactored to DemoList with useMemo

--- w05_HowReactReallyWorks/src/App.js
+++ w05_HowReactReallyWorks/src/App.js
@@ -1,26 +1,34 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useMemo } from 'react';
 
+// import DemoOutput from './components/Demo/DemoOutput';
+import DemoList from './components/Demo/DemoList';
 import Button from './components/UI/Button/Button';
-import DemoOutput from './components/Demo/DemoOutput';
 import './App.css';
 
 function App() {
-  const [showParagraph, setShowParagraph] = useState(false);
-  const [allowToggle, setAllowToggle] = useState(false);
+  // const [showParagraph, setShowParagraph] = useState(false);
+  // const [allowToggle, setAllowToggle] = useState(false);
+  // console.log('APP RUNNING');
+  // const toggleParagraphHandler = useCallback(() => {
+  //   if (allowToggle) setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  // }, [allowToggle]);
+  // const allowToggleHandler = () => { setAllowToggle(true); };
+  const [listTitle, setListTitle] = useState('My List');
 
-  console.log('APP RUNNING');
+  const changeTitleHandler = useCallback(() => {
+    setListTitle('New Title');
+  }, []);
 
-  const toggleParagraphHandler = useCallback(() => {
-    if (allowToggle) setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  }, [allowToggle]);
-  const allowToggleHandler = () => { setAllowToggle(true); };
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
 
   return (
     <div className="app">
-      <h1>Hi there!</h1>
+      {/* <h1>Hi there!</h1>
       <DemoOutput show={showParagraph} />
       <Button onClick={allowToggleHandler}>Allow Toggling</Button>
-      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button> */}
+      <DemoList title={listTitle} items={listItems} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
     </div>
   );
 }
