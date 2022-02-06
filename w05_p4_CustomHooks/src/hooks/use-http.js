6abfed638fea6971dcb17d97f3e0ef1ec297973e
Seth Bolander <bol15015@byui.edu>
2022-02-05 23:28:28 -0700

    updated use-http with useCallback

--- w05_p4_CustomHooks/src/hooks/use-http.js
+++ w05_p4_CustomHooks/src/hooks/use-http.js
@@ -1,22 +1,20 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
-const useHttp = (requestConfig, applyData) => {
+const useHttp = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const sendRequest = async () => {
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
     setIsLoading(true);
     setError(null);
     try {
       const response = await fetch(requestConfig.url, {
-        method: requestConfig.method,
-        headers: requestConfig.headers,
-        body: JSON.stringify(requestConfig.body),
+        method: requestConfig.method ? requestConfig.method : 'GET',
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
       });
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
+      if (!response.ok) throw new Error('Request failed!');
 
       const data = await response.json();
       applyData(data);
@@ -24,7 +22,7 @@ const useHttp = (requestConfig, applyData) => {
       setError(err.message || 'Something went wrong!');
     }
     setIsLoading(false);
-  };
+  }, []);
 
   return {
     isLoading,
