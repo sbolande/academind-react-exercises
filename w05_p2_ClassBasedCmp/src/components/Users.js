a850f2f411f0cbd5ec9a274aa859c408547e255c
Seth Bolander <bol15015@byui.edu>
2022-02-05 17:09:57 -0700

    Forgot bindings

--- w05_p2_ClassBasedCmp/src/components/Users.js
+++ w05_p2_ClassBasedCmp/src/components/Users.js
@@ -3,12 +3,6 @@ import { Component } from 'react';
 import User from './User';
 import classes from './Users.module.css';
 
-const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
-];
-
 class Users extends Component {
   constructor() {
     super();
@@ -28,7 +22,7 @@ class Users extends Component {
   render() {
     const usersList = (
       <ul>
-        {DUMMY_USERS.map((user) => (
+        {this.props.users.map((user) => (
           <User key={user.id} name={user.name} />
         ))}
       </ul>
@@ -36,10 +30,10 @@ class Users extends Component {
   
     return (
       <div className={classes.users}>
-        <button onClick={toggleUsersHandler}>
-          {showUsers ? 'Hide' : 'Show'} Users
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? 'Hide' : 'Show'} Users
         </button>
-        {showUsers && usersList}
+        {this.state.showUsers && usersList}
       </div>
     );
   }
