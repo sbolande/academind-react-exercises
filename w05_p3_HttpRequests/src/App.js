c92353c2ec7c54dc9ac07a8701b8579315a14f48
Seth Bolander <bol15015@byui.edu>
2022-02-05 17:49:10 -0700

    Added AddMovie cmp

--- w05_p3_HttpRequests/src/App.js
+++ w05_p3_HttpRequests/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
@@ -35,6 +36,8 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
+  const addMovieHandler = movie => { console.log(movie); };
+
   let content = <p>Found no movies.</p>;
 
   if (movies.length > 0) content = <MoviesList movies={movies} />;
@@ -43,6 +46,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
