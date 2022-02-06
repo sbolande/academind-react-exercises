7b2bafc69754faf8e1acbfc93a4d6c2ac1e99791
Seth Bolander <bol15015@byui.edu>
2022-02-05 17:21:53 -0700

    Implemented context consumer

--- w05_p2_ClassBasedCmp/src/components/UserFinder.js
+++ w05_p2_ClassBasedCmp/src/components/UserFinder.js
@@ -2,14 +2,11 @@ import { Fragment, useState, useEffect, Component } from 'react';
 
 import Users from './Users';
 import classes from './UserFinder.module.css';
-
-const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
-];
+import UsersContext from '../store/users-context';
 
 class UserFinder extends Component {
+    static contextType = UsersContext;
+
     constructor() {
         super();
         this.state = {
@@ -20,13 +17,15 @@ class UserFinder extends Component {
 
     componentDidMount() {
         // Send http request...
-        this.setState({ filteredUsers: DUMMY_USERS })
+        this.setState({ filteredUsers: this.context.users })
     }
 
     componentDidUpdate(prevProps, prevState) {
         if (prevState.searchTerm !== this.state.searchTerm) {
             this.setState({
-                filteredUsers: DUMMY_USERS.filter(user => user.name.includes(this.state.searchTerm)),
+                filteredUsers: this.context.users.filter(user => 
+                    user.name.includes(this.state.searchTerm)
+                ),
             });
         }
     }
@@ -39,9 +38,9 @@ class UserFinder extends Component {
         return (
             <Fragment>
                 <div className={classes.finder}>
-                    <input type='search' onChange={searchChangeHandler} />
+                    <input type='search' onChange={this.searchChangeHandler.bind(this)} />
                 </div>
-                <Users users={filteredUsers} />
+                <Users users={this.state.filteredUsers} />
             </Fragment>
         );
     }
