08890765de91cb70a42a71bd7437ae555dc26394
Seth Bolander <bol15015@byui.edu>
2022-02-05 17:19:58 -0700

    Created UsersContext

--- w05_p2_ClassBasedCmp/src/App.js
+++ w05_p2_ClassBasedCmp/src/App.js
@@ -1,10 +1,20 @@
 import UserFinder from './components/UserFinder';
+import Users from './components/Users';
+import UsersContext from './store/users-context';
+
+const DUMMY_USERS = [
+  { id: 'u1', name: 'Max' },
+  { id: 'u2', name: 'Manuel' },
+  { id: 'u3', name: 'Julie' },
+];
 
 function App() {
+  const UsersContext = { users: DUMMY_USERS };
+
   return (
-    <div>
+    <UsersContext.Provider value={UsersContext}>
       <UserFinder />
-    </div>
+    </UsersContext.Provider>
   );
 }
 
