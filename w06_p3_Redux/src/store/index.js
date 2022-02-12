1bd5712ef0bea02cc80415244bdc310aa67f805f
Seth Bolander <bol15015@byui.edu>
2022-02-11 22:18:09 -0700

    Added additional slice for auth

--- w06_p3_Redux/src/store/index.js
+++ w06_p3_Redux/src/store/index.js
@@ -7,25 +7,31 @@ const counterSlice = createSlice({
   name: 'counter',
   initialState,
   reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
+    increment(state) { state.counter++; },
+    decrement(state) { state.counter--; },
     increase(state, action) {
       state.counter = state.counter + action.payload;
     },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    }
+    toggleCounter(state) { state.showCounter = !state.showCounter; }
   }
 });
 
+const initialAuthState = { isAuthenticated: false, };
+
+const authSlice = createSlice({
+  name: 'authentication',
+  initialState: initialAuthState,
+  reducers: {
+    login(state) { state.isAuthenticated = true; },
+    logout(state) { state.isAuthenticated = false; },
+  },
+});
+
 const store = configureStore({
-  reducer: counterSlice.reducer
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
