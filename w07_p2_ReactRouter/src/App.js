0f6de6534273ddb5e876b299d231eab25914847b
Seth Bolander <bol15015@byui.edu>
2022-02-18 22:38:36 -0700

    Added exact and switch

--- w07_p2_ReactRouter/src/App.js
+++ w07_p2_ReactRouter/src/App.js
@@ -2,6 +2,7 @@ import { Route } from 'react-router-dom';
 
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
 import MainHeader from './components/MainHeader';
 
 function App() {
@@ -12,8 +13,11 @@ function App() {
         <Route path="/welcome">
           <Welcome />
         </Route>
-        <Route path="/products">
+        <Route path="/products" exact>
           <Products />
+        </Route>
+        <Route path='/products/:productId'>
+          <ProductDetail />
         </Route>
       </main>
     </div>
