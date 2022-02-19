41ad554ce679a92d13a8b10e7f8cb9e9cca18b52
Seth Bolander <bol15015@byui.edu>
2022-02-18 22:47:41 -0700

    forgot switch :)

--- w07_p2_ReactRouter/src/App.js
+++ w07_p2_ReactRouter/src/App.js
@@ -1,24 +1,26 @@
-import { Route } from 'react-router-dom';
+import { Route, Switch } from "react-router-dom";
 
-import Welcome from './pages/Welcome';
-import Products from './pages/Products';
-import ProductDetail from './pages/ProductDetail';
-import MainHeader from './components/MainHeader';
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import MainHeader from "./components/MainHeader";
 
 function App() {
   return (
     <div>
       <MainHeader />
       <main>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-        <Route path="/products" exact>
-          <Products />
-        </Route>
-        <Route path='/products/:productId'>
-          <ProductDetail />
-        </Route>
+        <Switch>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
