803327f8fc4e470110dc6f040a5cee60422bc5ed
Seth Bolander <bol15015@byui.edu>
2022-02-25 22:53:20 -0700

    added link

--- w09_NextJs/nextjs-course/pages/news/index.js
+++ w09_NextJs/nextjs-course/pages/news/index.js
@@ -1,5 +1,20 @@
 // our-domain.com/news/
 
+import Link from "next/link";
+import { Fragment } from "react";
+
 export default function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJS is a Great Framework
+          </Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </Fragment>
+  );
 }
