6bce471464bd55eb05edc8bf0558614eb76e49b8
Seth Bolander <bol15015@byui.edu>
2022-02-25 23:07:54 -0700

    added pages

---
+++ w09_NextJs/nextjs-course/pages/new-meetup/index.js
@@ -0,0 +1,10 @@
+// our-domain.com/new-meetup
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+export default function NewMeetupPage() {
+  const addMeetupHandler = (enteredMeetupData) => {
+    console.log(enteredMeetupData);
+  };
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+}
