b257ac6c057931ea92ae29c50551c526fb427f29
Seth Bolander <bol15015@byui.edu>
2022-02-25 22:59:03 -0700

    Converted to "bigger" starting project

--- w09_NextJs/nextjs-course/pages/index.js
+++
@@ -1,9 +0,0 @@
-// our-domain.com/
-
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-
-export default function HomePage() {
-  return <h1>The Home Page</h1>;
-}
